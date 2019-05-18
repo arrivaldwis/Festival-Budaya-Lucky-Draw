848a6f2b4ced9bd2707af250fd226e93f6549683
Arrival Sentosa <arrivaldwisentosa@gmail.com>
2019-05-18 11:31:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,41 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger('prod'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lucky Draw Fesbud' });
+});
 
 module.exports = app;
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on localhost:'+ port);
+
+/*var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();*/
+
+/*var express = require('express');
+var app = express();
+// Routes
+app.get('/', function(req, res) {
+res.send('Hello World, ini expressJS');
+});
+// Listen
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on localhost:'+ port);*/
